68ad1e12e7fc11c1f8a3d9c1703214199b20bc7a
harikapa <51950463+harikapa@users.noreply.github.com>
2019-07-19 09:53:16 +0530

    Update Movie.java

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -2,8 +2,10 @@ package main.java.com.stackroute.domain;
 
 public class Movie {
 
+    //Dependency to be injected 
     private Actor actor;
 
+    //setter injection
     public void setActor(Actor actor) {
         this.actor = actor;
     }
